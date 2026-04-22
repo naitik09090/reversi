56c66433bace5e75fb650bf25b7f426a8e6deeca
Naitik <naitikraiyani88@gmail.com>
2026-04-22 16:30:56 +0530

    fix Lighthouse error

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { LazyMotion, domAnimation } from 'framer-motion'
-import './index.css'
+import { LazyMotion } from 'framer-motion'
 import App from './App.jsx'
+
+// Load animation features asynchronously to reduce initial bundle size
+const loadFeatures = () => import('./utils/framerFeatures.js').then(res => res.default);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LazyMotion features={domAnimation}>
+    <LazyMotion features={loadFeatures} strict>
       <App />
     </LazyMotion>
   </StrictMode>,
